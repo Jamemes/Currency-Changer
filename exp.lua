4e4c9d214feec5fa60065bbeaee3b428d1419135
Jamemes <coderjames97@gmail.com>
2025-01-24 20:48:10 +0300

    2

--- exp.lua
+++ exp.lua
@@ -1,14 +1,9 @@
-local data = ExperienceManager.cash_string
 function ExperienceManager:cash_string(cash, cash_sign)
-	if not cash_sign or cash_sign == "$" then
-			local sign = ""
-			if cash < 0 then
-				sign = "-"
-			end
-
-		local final_cash_sign = type(cash_sign) == "string" and (cash_sign or tweak_data.cash_sign) or tweak_data.cash_sign
-		return sign .. final_cash_sign .. tweak_data:sep(cash * tweak_data.currency)
+	local sign = ""
+	if cash < 0 then
+		sign = "-"
 	end
 	
-	return data(self, cash, cash_sign)
+	local final_cash_sign = type(cash_sign) == "string" and (cash_sign or self._cash_sign) or self._cash_sign
+	return sign .. tweak_data:sep(cash, final_cash_sign)
 end
--- profile.lua
+++ profile.lua
@@ -1,27 +1,38 @@
-local url = "https://www.google.com/search?q=usd+to+"
+local url = "https://www.investing.com/currencies/usd-"
 local path = SavePath .. "currency.txt"
-
 local load_currency = io.open(path, 'r')
 if load_currency then
 	local save = json.decode(load_currency:read('*all'))
-	tweak_data.currency = save.currency or 1
-	tweak_data.cash_sign = save.cash_sign or "$"
-	tweak_data.currency_desc = save.currency_desc or ""
+	tweak_data.currency_data = {
+		currency = save.currency,
+		currency_name = save.currency_name,
+		currency_desc = save.currency_desc,
+		currency_format = save.currency_format or '[code] cash',
+		cash_sign = save.cash_sign,
+	}
+
 	load_currency:close()
 else
-	tweak_data.currency = 1
-	tweak_data.cash_sign = "$"
+	tweak_data.currency_data = {
+		currency_format = '[code] cash'
+	}
 end
 
-Hooks:Add("LocalizationManagerPostInit", "currency_changer_loc", function(...)
+Hooks:Add("LocalizationManagerPostInit", "currency_changer_loc", function(self)
 	LocalizationManager:add_localized_strings({
-		currency_desc = tweak_data.currency_desc or "",
-		currency_code = "Currency",
+		currency_desc = tweak_data.currency_data.currency_desc or '',
+		currency_code = "Currency code",
+		currency_format = "Currency format",
+		currency_changed_title = "Currency changed to ",
+		currency_loading_desc = "Loading data from Investing.com may take some time: from a few seconds to several minutes.\n\nMake sure the currency code is correct.\nWhen the currency changes, you will receive a notification.\n\nYou can close this message while the data is being loaded.",
 	})
 
 	if Idstring("russian"):key() == SystemInfo:language():key() then
 		LocalizationManager:add_localized_strings({
-			currency_code = "Валюта",
+			currency_code = "Код Валюты",
+			currency_format = "Формат валюты",
+			currency_changed_title = "Валюта изменена на ",
+			currency_loading_desc = "Загрузка данных с сайта Investing.com может занять некоторое время: от пары секунд до нескольких минут.\n\nУбедитесь, что код валюты введен верно.\nКогда валюта поменяется вы получите уведомвление.\n\nВы можете закрыть это сообщение пока ждете загрузку данных.",
 		})
 	end
 end)
@@ -35,14 +46,24 @@ local function save_data(tbl)
 end
 
 local function get_info(str, key1, key2)
-	local _, fst = string.find(str, key1)
-	local sec, _ = string.find(str, key2, fst)
+	local _, fst = string.find(str, key1, 0, true)
+	local sec, _ = string.find(str, key2, fst, true)
 	return string.sub(str, fst + 1, sec - 1)
 end
 
-function tweak_data:sep(total)
+function tweak_data:sep(total, cash_sign)
+	cash_sign = cash_sign or ""
+	
+	local mul = tweak_data.currency_data.currency and cash_sign ~= "" and tweak_data.currency_data.currency or 1
+	total = total * mul
+	
+	local function cash(money, code, name)
+		local cash_format = code and name and tweak_data.currency_data.currency_format or ''
+		return cash_sign ~= "" and cash_format ~= '' and cash_format:lower():gsub('cash', money):gsub('code', code):gsub('name', name) or cash_sign .. money
+	end
+	
 	if total < 1 then
-		return tostring(total)
+		return cash(tostring(total):sub(0, 4), tweak_data.currency_data.cash_sign, tweak_data.currency_data.currency_name)
 	end
 	
 	if string.find(math.round(total), "(%D+)") then
@@ -58,7 +79,7 @@ function tweak_data:sep(total)
 		s = s .. string.sub(reverse, i, i) .. (math.mod(i, 3) == 0 and i ~= string.len(reverse) and managers.localization:text("cash_tousand_separator") or "")
 	end
 
-	return string.reverse(s)
+	return cash(string.reverse(s), tweak_data.currency_data.cash_sign, tweak_data.currency_data.currency_name)
 end
 
 Hooks:Add("MenuManagerBuildCustomMenus", "_add_currency_code_item", function(menu_manager, nodes)
@@ -72,10 +93,11 @@ Hooks:Add("MenuManagerBuildCustomMenus", "_add_currency_code_item", function(men
 			text_id = "currency_code",
 			help_id = "currency_desc",
 			empty_gui_input_limit = 28,
+			input_limit = 5,
 			callback = "change_currency_call"
 		}
 		local new_item = node:create_item(data_node, params)
-		
+
 		new_item.dirty_callback = callback(node, node, "item_dirty")
 		if node.callback_handler then
 			new_item:set_callback_handler(node.callback_handler)
@@ -88,42 +110,135 @@ Hooks:Add("MenuManagerBuildCustomMenus", "_add_currency_code_item", function(men
 			end
 		end
 		
+		new_item:set_value(tweak_data.currency_data.cash_sign or "")
 		table.insert(node._items, pos, new_item)
+	
+		local params = {
+			callback = "change_currency_format_call",
+			name = "currency_format",
+			text_id = "currency_format",
+		}
+		
+		local vars = {
+			"cash",
+			"[code] cash",
+			"(code) cash",
+			"code cash",
+			"cash [code]",
+			"cash (code)",
+			"cash code",
+			"[name] cash",
+			"(name) cash",
+			"name cash",
+			"cash [name]",
+			"cash (name)",
+			"cash name",
+			"[code] cash (name)",
+			"(code) cash [name]",
+			"code cash name",
+			"[name] cash (code)",
+			"(name) cash [code]",
+			"name cash code",
+		}
+		
+		local data_node = {}
+		
+		for k, v in pairs(vars) do
+			table.insert(data_node, {
+				value = v,
+				text_id = v,
+				localize = false,
+				_meta = "option"
+			})
+		end
+		
+		data_node.type = "MenuItemMultiChoice"
+		local new_item = node:create_item(data_node, params)
+
+		new_item.dirty_callback = callback(node, node, "item_dirty")
+		if node.callback_handler then
+			new_item:set_callback_handler(node.callback_handler)
+		end
+		new_item:set_value(tweak_data.currency_data.currency_format)
+		table.insert(node._items, pos + 1, new_item)
 	end
 end)
-	
+
+function MenuCallbackHandler:change_currency_format_call(item)
+	tweak_data.currency_data.currency_format = item:value()
+	if tweak_data.currency_data.currency then
+		save_data(tweak_data.currency_data)
+		managers.menu_component:refresh_player_profile_gui()
+		managers.viewport:resolution_changed()
+	end
+end
+
 function MenuCallbackHandler:change_currency_call(item)
 	if not item._editing then
 		if item._input_text ~= "" then
-			local google = url .. item:input_text()
+			local google = url .. item:input_text():lower()
+			managers.system_menu:show({
+				text = managers.localization:text("currency_loading_desc"),
+				id = "loading_currency",
+				indicator = true,
+				button_list = {
+					{
+						cancel_button = false,
+						text = managers.localization:text("menu_back")
+					}
+				}
+			})
+			
 			dohttpreq(google, function(json_data)
 				local xml = json.encode(json_data)
-				local currency_desc = get_info(xml, [[BNeawe vvjwJb AP7Wnd\">1 USD to ]], [[- Xe]])
-				local currency_code = currency_desc:sub(0, 3)
-				local currency = get_info(xml, [[<div class=\"BNeawe iBp4i AP7Wnd\"><div><div class=\"BNeawe iBp4i AP7Wnd\">]], [[ ]]):gsub("(%D+)", function(s)
-					return s == [[,]] and [[.]] or ""
+				
+				local currency_desc = get_info(xml, [[USD ]], [[title]]):sub(0, -4)
+				local currency_code = item:input_text():upper()
+				local currency = get_info(xml, [[instrument-price-last]], [[div]]):gsub("(%D+)", function(s)
+					if s ~= [[.]] then
+						return ""
+					end
 				end)
-
+				
 				if currency and (currency ~= "" or not string.find(currency, "(%D+)")) then
-					tweak_data.currency = currency
-					tweak_data.cash_sign = string.format("[%s] ", currency_code)
-					item:set_value(currency_code)
-					local currency_desc = "1 USD = " .. currency .. " " .. currency_desc
+					local _, dollar = string.find(currency_desc, "Dollar ")
+					local cash_name = currency_desc:sub(dollar + 1, -17):gsub("to ", "")
+					currency_desc = "1 USD = " .. currency .. " " .. currency_desc
+					if managers.system_menu:is_active_by_id("loading_currency") then
+						managers.system_menu:close("loading_currency")
+					end
+					managers.system_menu:show({
+						title = managers.localization:text("currency_changed_title") .. currency_code,
+						text = currency_desc,
+						button_list = {
+							{
+								text = managers.localization:text("dialog_ok"),
+								cancel_button = true
+							}
+						}
+					})
+					
+					local currency_data = {
+						currency = currency,
+						cash_sign = currency_code,
+						currency_name = cash_name,
+						currency_format = tweak_data.currency_data.currency_format,
+						currency_desc = currency_desc,
+					}
+					save_data(currency_data)
+					tweak_data.currency_data = currency_data
+					
 					LocalizationManager:add_localized_strings({currency_desc = currency_desc})
+					
+					item:set_value(currency_code)
 					managers.menu_component:refresh_player_profile_gui()
 					managers.viewport:resolution_changed()
-					save_data({
-						currency = tweak_data.currency,
-						cash_sign = tweak_data.cash_sign,
-						currency_desc = currency_desc
-					})
 				end
 			end)
 		else
-			tweak_data.currency = 1
-			tweak_data.cash_sign = "$"
+			tweak_data.currency_data = {}
+			save_data(tweak_data.currency_data)
 			LocalizationManager:add_localized_strings({currency_desc = ""})
-			save_data({})
 			managers.menu_component:refresh_player_profile_gui()
 			managers.viewport:resolution_changed()
 		end
